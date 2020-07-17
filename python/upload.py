5a6c089250b0cc1e1c0adc298d646e37f61f47b6
Yael Brown <yaelrbrown@gmail.com>
2020-07-16 23:14:05 -0500

    add gitignore

---
+++ python/upload.py
@@ -0,0 +1,15 @@
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+cloudinary.config(
+  cloud_name = "",
+  api_key = "",
+  api_secret = ""
+)
+
+print("Change the config to get this to work and delete this line")
+
+file = "/Users/yaelbrown/Projz/python_image_db/sample_images/rough-day-bird.jpg"
+
+# cloudinary.uploader.upload(file)
